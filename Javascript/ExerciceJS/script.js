7f1c32df7eed654f2375bb08f1dc65b195c3560c
Niyomwungere-Richard <niyomwungererichard59@gmail.com>
2026-04-21 10:12:31 +0200

    Creation des exercices et en javascript

---
+++ Javascript/ExerciceJS/script.js
@@ -0,0 +1,25 @@
+// les Variable en Javascript 
+let Nom="Niyomwungere";
+let Prenom="Richard";
+const pays ="Burundi";
+let age=30;
+const PI = 3.14;
+console.log(`Mon pays natale c'est le ${pays}`);
+
+console.log(`Le Premiere Variable que j'ai cree c'est ${Nom}`);
+console.log(`Voici la variable qui contient un nombre ${age}`)
+
+//les types des donnees en Javascript 
+//2 number 
+//3. Boolen
+// -----les String 
+
+let kenya = "Le pays d'afrique de l'EAC"
+const Mystring = "My Name";
+
+console.log(Mystring);
+let  HelloSentence ="Hello "+ Nom
+console.log(HelloSentence);
+
+//Avec les `back-tick `
+let MyName=`This is my name ${Nom} ${Prenom }`
